0be2a16aab6335fd96b10c2a535d6033c1c0e528
marieglennalano <marieglennalano@gmail.com>
2025-06-12 14:37:40 +0800

    MG

--- amihan-booking-airlines/src/App.js
+++ amihan-booking-airlines/src/App.js
@@ -1,13 +1,33 @@
-
-
-
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
+import AppNavbar from './components/AppNavbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Book from './pages/Book';
+import Contacts from './pages/Contacts';
+import Pages from './pages/Pages';
+// import Login from './pages/Login';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <UserProvider>
+        <Router>
+          <AppNavbar />
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/book" element={<Book />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/pages" element={<Pages />} />
+              {/* <Route path="/login" element={<Login />} /> */}
+            </Routes>
+          </Container>
+        </Router>
+      </UserProvider>
+    </>
   );
 }
 
