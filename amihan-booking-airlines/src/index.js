7f4152bcc570f2250bb29889166d08331a80ef7e
dejesusjerico528 <dejesusjerico528@gmail.com>
2025-06-15 16:43:19 +0800

    added code

--- amihan-booking-airlines/src/index.js
+++ amihan-booking-airlines/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+// Page
+import AboutUs from './pages/AboutUs/AboutUs';
+import Deals from './pages/Deals/Deals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
-);
+    <Deals />
+    <AboutUs />
+  
+  </>
+)
 
